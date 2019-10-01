1c966d492a4d129e314c4d0d3b83f9edad9ef15d
Dennis Stritzke <dennis@stritzke.me>
2019-10-01 14:20:37 +0200

    Setting `string` and `[]string` fields.

--- bind.go
+++ bind.go
@@ -5,6 +5,10 @@ import (
 	"reflect"
 )
 
+const (
+	tagIdentifier = "header"
+)
+
 // An InvalidBindError describes an invalid argument passed to Bind.
 // (The argument to Bind must be a non-nil pointer.)
 type InvalidBindError struct {
@@ -30,5 +34,48 @@ func Bind(r http.Request, v interface{}) error {
 		return &InvalidBindError{reflect.TypeOf(v)}
 	}
 
+	return bind(v, r)
+}
+
+func bind(v interface{}, r http.Request) error {
+	rv := reflect.Indirect(reflect.ValueOf(v))
+	t := reflect.TypeOf(v).Elem()
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+
+		if headerName, ok := field.Tag.Lookup(tagIdentifier); ok {
+			headerValue := r.Header[headerName]
+
+			if len(headerValue) > 0 {
+				value := rv.Field(i)
+				setValue(field, value, headerValue)
+			}
+		}
+	}
+
 	return nil
+}
+
+func setValue(field reflect.StructField, value reflect.Value, headerValue []string) {
+	switch field.Type.Kind() {
+	case reflect.String:
+		setStringValue(value, headerValue)
+	case reflect.Slice:
+		setSliceValue(value, headerValue)
+	}
+}
+
+func setSliceValue(value reflect.Value, headerValue []string) {
+	headerValueCount := len(headerValue)
+	slice := reflect.MakeSlice(reflect.TypeOf([]string{}), headerValueCount, headerValueCount)
+	for i := 0; i < headerValueCount; i++ {
+		sliceItem := slice.Index(i)
+		sliceItem.SetString(headerValue[i])
+	}
+	value.Set(slice)
+}
+
+func setStringValue(value reflect.Value, headerValue []string) {
+	value.SetString(headerValue[0])
 }
--- bind_test.go
+++ bind_test.go
@@ -5,6 +5,21 @@ import (
 	"testing"
 )
 
+func createRequest(omit ...string) http.Request {
+	headers := map[string][]string{
+		"Foo": {"Bar"},
+		"Bar": {"Foo", "42"},
+	}
+
+	for _, headerToOmit := range omit {
+		delete(headers, headerToOmit)
+	}
+
+	return http.Request{
+		Header: headers,
+	}
+}
+
 func TestBind_nil(t *testing.T) {
 	err := Bind(http.Request{}, nil)
 	checkForInvalidBindError(t, err)
@@ -13,6 +28,53 @@ func TestBind_nil(t *testing.T) {
 func TestBind_nonPointerType(t *testing.T) {
 	err := Bind(http.Request{}, "")
 	checkForInvalidBindError(t, err)
+}
+
+func TestBind_String(t *testing.T) {
+	type Test struct {
+		Foo string `header:"Foo"`
+	}
+
+	var test Test
+
+	err := Bind(createRequest(), &test)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	expected := "Bar"
+	actual := test.Foo
+	if actual != expected {
+		t.Errorf("Expected field to have value '%s', but got '%s'...", expected, actual)
+	}
+}
+
+func TestBind_StringSlice(t *testing.T) {
+	type Test struct {
+		Bar []string `header:"Bar"`
+	}
+
+	var test Test
+
+	err := Bind(createRequest(), &test)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if len(test.Bar) != 2 {
+		t.Errorf("Expected two values, but got %d: %s", len(test.Bar), test.Bar)
+		return
+	}
+
+	if test.Bar[0] != "Foo" {
+		t.Errorf("Expected first element to be 'Foo', but was '%s'...", test.Bar[0])
+	}
+
+	if test.Bar[1] != "42" {
+		t.Errorf("Expected first element to be '42', but was '%s'...", test.Bar[1])
+	}
 }
 
 func checkForInvalidBindError(t *testing.T, err error) {
